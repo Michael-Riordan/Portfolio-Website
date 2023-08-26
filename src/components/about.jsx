3c7bb5cd29c8bfb9fa6cf2207bc5748c87618b5b
Michael <michael.riordan81@outlook.com>
2023-08-26 10:39:42 -0700

    add about me section content

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,8 +4,34 @@ export default function About() {
         <>
             <section id='about-body'>
                 <header id='about-header'>
-                    <h1 id='about-header-content'>Problem Solver. Resilient. Adaptable.</h1>
-                    <p id='about-description'>The above three traits have followed me throughout all of my life.</p>
+                    <h1 id='about-header-content'>Problem Solving. Resilience. Adaptability.</h1>
+                    <p id='about-description'>
+                    My journey leading up to becoming a Software Developer has been a tapestry of diverse experiences 
+                    that have profoundly shaped my approach to problem-solving, resilience, and adaptability.
+                    <br/> <br/>
+                    
+                    From tackling the formidable challenges of the hospitality industry to 
+                    conquering the rugged terrains of the Pacific Crest Trail, 
+                    I've cultivated a knack for creative problem-solving. 
+                    With a backpack as my only companion and a tent as my shelter, 
+                    I navigated through uncertainty, making critical decisions that shaped my journey. 
+                    <br/> <br/>
+
+                    Further enhancing my resilience, I spent seven months traversing the vast expanse 
+                    of the US in a self-built camper van. Embracing the nomadic lifestyle, 
+                    I navigated diverse landscapes, weathered unexpected obstacles, and learned to adapt 
+                    to new environments with unwavering determination.
+                    <br/> <br/>
+
+                    This eclectic range of experiences has instilled in me a profound adaptability—an essential 
+                    trait in a field as dynamic as software development. 
+                    These experiences have taught me the value of staying resilient and adapting to new environments—an 
+                    invaluable trait in the dynamic field of software development.
+                    <br/> <br/>
+
+                    As I embark on my journey in the world of software, I am excited to leverage my unique experiences 
+                    to contribute to the success of the team and create meaningful contributions to the projects ahead.
+                    </p>
                 </header>
             </section>
         </>
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
             <nav id='portfolio-nav-bar'>
                 <Link to='/' id='personal-logo' >MJR</Link>
                 <div id='links'>
-                    <Link to='/About' id='about-link'>About</Link>
+                    <Link to='/About' id='about-link'>About Me</Link>
                     <a href="https://github.com/Michael-Riordan" id='github-link'>GitHub</a>
                 </div>
             </nav>
